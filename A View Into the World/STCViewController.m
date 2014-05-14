d7cdf0ef79b337c6cdd19fef0a6d4a069f0d5418
StefenClausen <stefanclaussen@me.com>
2014-05-14 16:42:36 +0100

    Complete ending lecture 210

--- A View Into the World/STCViewController.m
+++ A View Into the World/STCViewController.m
@@ -1,10 +1,6 @@
 //
 //  STCViewController.m
 //  A View Into the World
-//
-//  Created by Stefan Claussen on 14/05/2014.
-//  Copyright (c) 2014 One foot after the other. All rights reserved.
-//
 
 #import "STCViewController.h"
 
@@ -18,6 +14,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+//    NSLog(@"button1 button size (%f, %f", self.button1.bounds.size.height, self.button1.bounds.size.width);
+//    NSLog(@"button1 frame size (%f, %f", self.button1.frame.size.height, self.button1.frame.size.width);
+//    NSLog(@"button1 bounds origin (%f, %f", self.button1.bounds.origin.x, self.button1.bounds.origin.y);
+//    NSLog(@"button1 frame origin (%f, %f", self.button1.frame.origin.x, self.button1.frame.origin.y);
+//    NSLog(@"button1 center (%f, %f)", self.button1.center.x, self.button1.center.y);
+    
+    NSLog(@"view1 button size (%f, %f", self.view1.bounds.size.height, self.view1.bounds.size.width);
+    NSLog(@"view1 frame size (%f, %f", self.view1.frame.size.height, self.view1.frame.size.width);
+    NSLog(@"view1 bounds origin (%f, %f", self.view1.bounds.origin.x, self.view1.bounds.origin.y);
+    NSLog(@"view1 frame origin (%f, %f", self.view1.frame.origin.x, self.view1.frame.origin.y);
+    NSLog(@"view1 center (%f, %f)", self.view1.center.x, self.view1.center.y);
+
+    NSLog(@"button2 button size (%f, %f", self.button2.bounds.size.height, self.button2.bounds.size.width);
+    NSLog(@"button2 frame size (%f, %f", self.button2.frame.size.height, self.button2.frame.size.width);
+    NSLog(@"button2 bounds origin (%f, %f", self.button2.bounds.origin.x, self.button2.bounds.origin.y);
+    NSLog(@"button2 frame origin (%f, %f", self.button2.frame.origin.x, self.button2.frame.origin.y);
+    NSLog(@"button2 center (%f, %f)", self.button2.center.x, self.button2.center.y);
+
 }
 
 - (void)didReceiveMemoryWarning
